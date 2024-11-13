401b80ea2ee1552231e99cb592a2cc3129100ffb
KattekolaSreeVidya <kattekolasreevidya@gmail.com>
2024-11-13 16:25:49 +0530

    feat: add ReadMore component and update BlogPost

--- src/components/BlogPost/BlogPost.jsx
+++ src/components/BlogPost/BlogPost.jsx
@@ -1,31 +1,46 @@
+// src/components/BlogPost/BlogPost.jsx
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import styles from './BlogPost.module.css';
+import LikeButton from '../LikeButton/LikeButton';
+import CommentSection from '../CommentSection/CommentSection';
+import { calculateReadTime } from '../../utils/readTime';
+import ReadMore from '../ReadMore/ReadMore'; // Importing the new ReadMore component
+import './BlogPost.module.css';
 
-function BlogPost({ title, content, author, date, readTime }) {
+const BlogPost = ({ id, title, content, author, date }) => {
+  const [readTime, setReadTime] = useState(0);
+
+  useEffect(() => {
+    setReadTime(calculateReadTime(content));
+  }, [content]);
+
   return (
-    <article className={styles.blogPost}>
-      <div className={styles.header}>
-        <h2 className={styles.title}>{title}</h2>
-        <div className={styles.meta}>
-          <span className={styles.author}>By {author}</span>
-          <time className={styles.date}>{date}</time>
-          <span className={styles.readTime}>{readTime} min read</span>
+    <article className="blog-post">
+      <div className="blog-post__header">
+        <h2 className="blog-post__title">{title}</h2>
+        <div className="blog-post__meta">
+          <span className="blog-post__author">By {author}</span>
+          <time className="blog-post__date">{new Date(date).toLocaleDateString()}</time>
+          <span className="blog-post__read-time">{readTime} min read</span>
         </div>
       </div>
-      
-      <div className={styles.content}>
-        {content}
+
+      <div className="blog-post__content">
+        <ReadMore content={content} /> {/* Using ReadMore component */}
       </div>
+
+      <LikeButton initialLikes={0} />
+      <CommentSection postId={id} />
     </article>
   );
-}
+};
 
 BlogPost.propTypes = {
+  id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   content: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
-  date: PropTypes.string.isRequired,
-  readTime: PropTypes.number.isRequired
+  date: PropTypes.string.isRequired
 };
 
 export default BlogPost;
